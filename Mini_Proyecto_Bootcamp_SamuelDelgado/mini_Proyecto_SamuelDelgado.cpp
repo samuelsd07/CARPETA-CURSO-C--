7a97ac297afb2099e99dadad752303b6fdc67e67
Samuel D. <delgadochaconsamueljose@gmail.com>
2026-09-14 18:59:40 -0500

    Validaciones de MOVECARD y Casos borde

--- Mini_Proyecto_Bootcamp_SamuelDelgado/mini_Proyecto_SamuelDelgado.cpp
+++ Mini_Proyecto_Bootcamp_SamuelDelgado/mini_Proyecto_SamuelDelgado.cpp
@@ -75,3 +75,35 @@ void mostrarMenu() {
     cout << "Seleccione una opcion: ";
 }
 
+// 2. Registro de tarjetas:
+void registrarTarjeta(string codigos[], double saldos[], int tipos[], int &total) {
+    // Validacion de caso borde: Limite maximo de 20 registros
+    if (total >= max_registro) {
+        cout << "\nNo se pueden agregar mas tarjetas. Limite alcanzado (20).\n";
+        return;
+    }
+
+    cout << "\n--- NUEVO REGISTRO DE TARJETA [" << (total + 1) << "/20] ---\n";
+    cout << "Ingrese el codigo UID de la tarjeta (ej. MC-101): ";
+    cin >> codigos[total];
+
+    // Validacion para el saldo.
+    cout << "Ingrese el saldo inicial ($): ";
+    cin >> saldos[total];
+    while (saldos[total] < 0.0 || saldos[total] > 100.0) {
+        cout << "[ERROR] El saldo debe estar entre $0.00 y $100.00:$";
+        cin >> saldos[total];
+    }
+
+    // Validacion para las diferentes tarifas.
+    cout << "Seleccione tipo (1: General, 2: Estudiantil, 3: Tercera Edad): ";
+    cin >> tipos[total];
+    while (tipos[total] < 1 || tipos[total] > 3) {
+        cout << "[ERROR] Tipo no valido. Ingrese 1, 2 o 3: ";
+        cin >> tipos[total];
+    }
+
+    total++;
+    cout << ">> ¡Tarjeta registrada exitosamente!\n";
+}
+
