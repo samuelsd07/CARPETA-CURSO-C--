b92ccc83ecfbb126044b6fdb06197b9a953bd662
Samuel D. <delgadochaconsamueljose@gmail.com>
2026-09-19 15:16:30 -0500

    Programa de Clase Mascota terminado - Samuel Delgado

--- JAVA/Tarea_Clase10_SamuelDelgado_/src/App.java
+++ JAVA/Tarea_Clase10_SamuelDelgado_/src/App.java
@@ -8,8 +8,8 @@ Fecha: 19-09-2026.
 public class App {
     public static void main(String[] args) {
         // 5. Crear dos objetos de tipo Mascota
-        Mascota mascota1 = new Mascota("Firulais", "Perro", 3);
-        Mascota mascota2 = new Mascota("Oso", "Gato", 2);
+        Mascota mascota1 = new Mascota("Firulais", "Perro", 10);
+        Mascota mascota2 = new Mascota("Oso", "Gato", 9);
 
         // 6. Mostrar información de las mascotas en consola
         mascota1.mostrarInfo();
